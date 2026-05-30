255648aac0d23b67f8cc0688af28d5e5cc25c11a
akhilyeduresi <akhil.yeduresi@gmail.com>
2026-05-30 23:23:55 +0100

    port 8080

--- main.py
+++ main.py
@@ -23,3 +23,9 @@ def predict(data: HouseInput):
     features = np.array([[data.rooms, data.size, data.age]])
     prediction = model.predict(features)[0]
     return {"prediction": round(prediction, 2)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
